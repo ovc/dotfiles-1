0f844bada369600d1688b6513e3b95118231b436
Erik Westrup <erik.westrup@gmail.com>
2012-06-29 21:27:44 +0200

    KDE stuff.

--- .kde4/Autostart/autostart_kde.sh
+++ .kde4/Autostart/autostart_kde.sh
@@ -2,11 +2,11 @@
 # Programs that should be started with each KDE session.
 
 # Start fetching mails.
-/usr/bin/env offlineimap
+/usr/bin/env offlineimap &
 
 # Start Yakuake and load my tmux session.
 /usr/bin/env yakuake
-yakuake_sessionID=`qdbus org.kde.yakuake /yakuake/sessions org.kde.yakuake.activeSessionId`
+yakuake_sessionID=$(qdbus org.kde.yakuake /yakuake/sessions org.kde.yakuake.activeSessionId)
 qdbus org.kde.yakuake /yakuake/tabs setTabTitle $yakuake_sessionID "irctor"
 qdbus org.kde.yakuake /yakuake/sessions runCommandInTerminal $yakuake_sessionID "irctor attach"
 
@@ -28,6 +28,9 @@ led=c					# LED to blink c(aps lock), s(croll lock) or n(um pad)
 
 # Start irexec used for executing arbitary commands from ~/.lircrc.
 /usr/bin/env irexec --daemon
+
+# Start dropbox sync.
+/usr/bin/env dropbox start -i
 
 # Start notify-listener to the irssi plugin notify can emit messages.
 # NOTE stoped working, missing typelibe, so the legacy notify-send is used directly in the irssi plugin instead.
