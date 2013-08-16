3d6b1d4d5fe5a61d0bf7d092858830a37de36a60
Erik Westrup <erik.westrup@gmail.com>
2013-08-16 23:44:25 +0200

    Clean up old configs.

--- .kde4/env/my_envs.sh
+++ .kde4/env/my_envs.sh
@@ -5,5 +5,5 @@
 export GPODDER_HOME="$HOME/media/music/gPodder"
 
 # Let programs know where to connect to MPD.
-export MPD_HOST="rJotJsq2996iZjWFxCiJEqGeAYh_hnNq@localhost"
+export MPD_HOST="somepasswordwhenIhaveaKDEmachineagain@localhost"
 export MPD_PORT="6600"
