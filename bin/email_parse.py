4ad1092015024998ee58e7e31e9577d787a1c413
Erik Westrup <erik.westrup@gmail.com>
2013-09-25 00:09:00 +0200

    Finally updated the maildir watch script to use a email parsing library to get the real decoded email body from multi part emails.

---
+++ bin/email_parse.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# Get conents from an input email file.
+
+import argparse
+import email
+import sys
+
+import html2text
+
+def _decode_bytes(baffelblag):
+        if type(baffelblag) is bytes:
+            codecs = ['utf8', 'iso-8859-1']
+            for codec in codecs:
+                try:
+                    return baffelblag.decode(codec)
+                except:
+                    pass
+        else:
+            return baffelblag
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Get info from an input email.")
+    parser.add_argument('email_file', type=argparse.FileType('r'), help='The email file to read from.')
+    cmd_grop = parser.add_mutually_exclusive_group(required=True)
+    cmd_grop.add_argument('--fromf', action='store_true', help='The "From:" field.')
+    cmd_grop.add_argument('--subject', action='store_true', help='The "Subject:" field.')
+    cmd_grop.add_argument('--body', action='store_true', help='The body.')
+
+    args = parser.parse_args()
+    return args.email_file, args.fromf, args.subject, args.body
+
+def main():
+    email_file, fromf, subject, body = parse_args()
+
+    message = email.message_from_file(email_file)
+    #print(message)
+    if fromf:
+        from_str = _decode_bytes(email.header.decode_header(message['from'])[0][0])
+        print("{:s}".format(from_str))
+    elif subject:
+        subject_str = _decode_bytes(email.header.decode_header(message['subject'])[0][0])
+        print("{:s}".format(subject_str))
+    elif body:
+        for part in message.walk():
+            if part.get_content_type() == 'text/plain':
+                body_str = _decode_bytes(part.get_payload(decode=True))
+                break
+            elif part.get_content_type() == 'text/html':
+                body_str = _decode_bytes(part.get_payload(decode=True))
+                h2txt = html2text.HTML2Text()
+                h2txt.ignore_links = True
+                #h2txt.body_width = 80
+                body_str = h2txt.handle(body_str)
+
+        print("{:s}".format(body_str))
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
--- bin/maildir_watch.sh
+++ bin/maildir_watch.sh
@@ -6,8 +6,8 @@ maildir_path="$HOME/.mail"		# Path to Maildir root.
 mailboxes=(inbox lists)			# Mailboxes to watch.
 watchdirs=$(for box in ${mailboxes[*]}; do echo ${maildir_path}/$box/new; done)
 scriptname=${0##*/}
-multicut_file="/tmp/${scriptname}_${USER}_multipart_cut.txt"
-trimmed_file="/tmp/${scriptname}_${USER}_trimmed.txt"
+#multicut_file="/tmp/${scriptname}_${USER}_multipart_cut.txt"
+#trimmed_file="/tmp/${scriptname}_${USER}_trimmed.txt"
 
 trim() {
     local var="$1"
@@ -20,59 +20,47 @@ parse_send() {
 	local inbox="$1"
 	local mailfile="$2"
 
-	# Get subject and trim length.
-	local subject=$(grep -i "Subject: " "$mailfile")
+	# Get parts. Decodes and does MIME, HTML parsing etc.
+	local subject=$(email_parse.py --subject "$mailfile")
+	local from=$(email_parse.py --from "$mailfile")
+	local body=$(email_parse.py --body "$mailfile")
+
+	# Subject length trimming.
 	subject=${subject:0:40}
 	subject=$(trim "$subject")
 
-	# Get from field and display name or email.
-	local from_row=$(grep -i "^From:" "$mailfile" | sed 's/From:\s*//I')
-	local from_name=$(echo "$from_row" | grep -Po "[^<>]+(?=(?:<|$))")
-	local from_email=$(echo "$from_row" | grep -Po "(?<=<).+(?=>)")
-	local from="From: "
+
+	# Prefer display name to email address.
+	local from_name=$(echo "$from" | grep -Po "[^<>]+(?=(?:<|$))")
+	local from_email=$(echo "$from" | grep -Po "(?<=<).+(?=>)")
 	if [ -n "$from_name" ]; then
-		from="${from}${from_name}"
+		from="$from_name"
 	elif [ -n "$from_email" ]; then
-		from="${from}${from_email}"
+		from="$from_email"
 	else
-		from="${from}<unknown>"
+		from="<unknown>"
 	fi
+
+	# From length trimming.
 	from=${from:0:30}
 	from=$(trim "$from")
 
-	# Parse out body.
-	# Scroll down to body.
-	local body=$(sed '1,/^$/d' "$mailfile")
-	# Remove content lines.
-	body=$(echo "$body" | grep -v "^Content-.*:")
-	# Remove random ID line.
-	body=$(echo "$body" |  grep -v "^--[[:xdigit:]]\+")
-
-	# Get rid of multipart crap.
-	# TODO comm needs sorted input... re-do and do it right.
-	echo "$body" | sed  -n '/------=_Part_/,/^$/p'  > "$multicut_file"
-	echo "$body" > "$trimmed_file"
-	body=$(comm -23 "$trimmed_file" "$multicut_file")
-
+	# Work your body.
 	# Strip signature.
 	body=$(echo "$body" | sed -n '/-- /q;p')
-	# Convert HTML to ASCII.
-	boyd=$(echo "$body" | html2text)
-	# Remove empty lines.
-	body=$(echo "$body" | sed '/^ *$/d')
 	# Join lines.
-	body=$(echo "$body" | tr "\\n" ' ')
-	# Substitute repedetive spaces to get more text.
-	body=$(echo "$body" | sed 's/\s\s*/ /g')
-	# Strim leading and trailing spaces.
+	body=$(echo "$body" | tr --squeeze-repeats "\\n" ' ')
+	# Squeeze repetitive spaces to get more text.
+	body=$(echo "$body" | sed 's/\s\s\+/ /g')
+	# Strip leading and trailing spaces.
 	body=$(trim "$body")
 	# Limit length.
 	body=${body:0:100}
 
-	# Notify summary string.
-	local out_summary=$(printf "[%s] %s | %s |\\n" "$inbox" "$from" "$subject")
-	# Notify body string.
+	# Format the output.
+	local out_summary=$(printf "[%s] From: %s | Subject: %s |\\n" "$inbox" "$from" "$subject")
 	local out_body=$(printf "Body: %s [...]\\n" "$body")
+
 	# Send the message with the name this scrip was invoked with.
 	notify-send --app-name "$scriptname" "$out_summary" "$out_body"
 }
