b431695ecb3081ed614510ffd922346ef0783e0b
Erik Westrup <erik.westrup@gmail.com>
2014-03-24 23:19:27 +0100

    pulseaudio starting, scanimage fix and .spl file removal.

--- bin/scan_dcp7070dw.sh
+++ bin/scan_dcp7070dw.sh
@@ -2,24 +2,12 @@
 # Scan image from Brother DCP-7070DW scanner.
 # dependencies: sane, brscan4 (from AUR)
 # install: sudo brsaneconfig4 -a name=dcp7070dw model=DCP-7070DW ip=10.0.0.5
-# Device name found with $(scanimage -L).
+# Device name found with $(scanimage -L) after configuring it with brsaneconfig4.
 
 dev_name="brother4:net1;dev0"
-out_path="$HOME/media/images/scanned"
-prefix="scanned_"
-
-next_scan_number() {
-	local prefix="$1"
-	local next
-	local files=$(ls ${prefix}[0-9]* 2>/dev/null)
-	if [ "$?" -eq 0 ]; then
-		last=$(echo "$files" | sed -e "s|${prefix}||" -e "s|\.pdf||" | sort -nr | head -1)
-		next=$((last + 1))
-	else
-		next=0
-	fi
-	echo "$next"
-}
+dest="$HOME/media/images/scanned"
+timestamp=$(date "+%Y-%m-%d-%H%M%S")
+scan_name="scanned_${timestamp}"
 
 convert2type() {
 	local scan_name="$1"
@@ -34,19 +22,17 @@ copy_path_to_xclipboard() {
 	echo  "$path" | xclip -i
 }
 
-cd "$out_path"
-next=$(next_scan_number "$prefix")
-scan_name="${prefix}${next}"
-scanimage --device-name="${dev_name}" --progress --format=tff > "${scan_name}.tff"
+scanimage --device-name="${dev_name}" --progress --format=tff > "/tmp/${scan_name}.tff"
 if [ "$?"  -ne 0 ]; then
 	echo "scanimage(1) failed." 1>&2
 	exit
 fi
 
-if [ -e "${scan_name}.tff" ]; then
-	convert2type "$scan_name" "tff" "pdf"
-	copy_path_to_xclipboard "${out_path}/${scan_name}.pdf"
-	echo "Scanned image at: ${out_path}/${scan_name}.pdf"
+if [ -e "/tmp/${scan_name}.tff" ]; then
+	convert2type "/tmp/$scan_name" "tff" "pdf"
+	mv "/tmp/${scan_name}.pdf" $dest
+	copy_path_to_xclipboard "${dest}/${scan_name}.pdf"
+	echo "Scanned image at: ${dest}/${scan_name}.pdf"
 else
 	echo "Scan failed -- no output." 1>&2
 fi
