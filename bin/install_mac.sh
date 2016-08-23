9f9f1477116e3154e732b8b763c0ab58cc66ed57
Erik Westrup <erik.westrup@gmail.com>
2016-08-23 20:37:55 +0200

    Collected changes

--- bin/install_mac.sh
+++ bin/install_mac.sh
@@ -5,57 +5,57 @@ make_1line() {
 }
 
 read -r -d '' brew_apps_default <<'EOAPPS'
-ack
-aspell
-cloc
-cmatrix
-cowsay
-cscope
-ctags
-ctags
-curl
-curl
-dfc
-dos2unix
-elinks
-emacs
-git
-git
-graphviz
-htop-osx
-htop-osx
-httpie
-imagemagick
-irssi
-links
-macvim
-macvim
-pdfgrep
-pyenv
-pyenv-virtualenvwrapper
-python
-python3
-readline
-reattach-to-user-namespace
-source-highlight
-task
-task
-tig
-tig
-tmux
-tmux
-tree
-tree
-urlview
-vim
-vim
-watch
-watch
-wget
-zsh
+	ack
+	aspell
+	cloc
+	cmatrix
+	cowsay
+	cscope
+	ctags
+	ctags
+	curl
+	curl
+	dfc
+	dos2unix
+	elinks
+	emacs
+	git
+	git
+	graphviz
+	htop-osx
+	htop-osx
+	httpie
+	imagemagick
+	ipython
+	irssi
+	links
+	macvim
+	macvim
+	pdfgrep
+	pyenv
+	pyenv-virtualenvwrapper
+	python
+	python3
+	readline
+	reattach-to-user-namespace
+	source-highlight
+	task
+	tasksh
+	tig
+	tig
+	tmux
+	tmux
+	tree
+	tree
+	urlview
+	vim
+	vim
+	watch
+	watch
+	wget
+	zsh
 EOAPPS
 brew_apps_default=$(make_1line "$brew_apps_default")
-
 
 read -r -d '' brew_apps_additional <<'EOAPPS'
 EOAPPS
@@ -63,54 +63,54 @@ brew_apps_additional=$(make_1line "$brew_apps_additional")
 
 
 read -r -d '' cask_apps_default <<'EOAPPS'
-appcleaner
-clamxav
-colloquy
-cyberduck
-dropbox
-firefox
-flip4mac
-flux
-gimp
-google-chrome
-gpgtools
-handbrake
-iterm2
-jing
-libreoffice
-mactex
-macvim
-name-mangler
-postman
-skype
-spotify
-spotify-notifications
-the-unarchiver
-transmission
-vlc
-wireshark
+	appcleaner
+	clamxav
+	colloquy
+	cyberduck
+	dropbox
+	firefox
+	flip4mac
+	flux
+	gimp
+	google-chrome
+	gpgtools
+	handbrake
+	iterm2
+	jing
+	libreoffice
+	mactex
+	macvim
+	name-mangler
+	postman
+	skype
+	spotify
+	spotify-notifications
+	the-unarchiver
+	transmission
+	vlc
+	wireshark
 EOAPPS
 cask_apps_default=$(make_1line "$cask_apps_default")
 
 read -r -d '' cask_apps_additional <<'EOAPPS'
-1password
-adium
-caffeine
-chicken
-eclipse-ide
-electric-sheep
-google-drive
-karabiner
-keepassx
-perian
-pycharm
-seil
-skim
-slack
-thunderbird
-virtualbox
-xee
-yasu
+	1password
+	adium
+	caffeine
+	chicken
+	eclipse-ide
+	electric-sheep
+	google-drive
+	karabiner
+	keepassx
+	perian
+	pycharm
+	seil
+	skim
+	slack
+	thunderbird
+	virtualbox
+	xee
+	yasu
 EOAPPS
 cask_apps_additional=$(make_1line "$cask_apps_additional")
 
---
+++ bin/osx_config.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+# Change screenshot destination from Desktop to sane
+defaults write com.apple.screencapture location ~/media/images/screenshots
